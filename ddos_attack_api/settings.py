4a21dad14fba7f04f75ccde6184e8e11cf3b693b
Deepesh Paryani <paryanideepesh@gmail.com>
2023-05-15 12:58:53 +0530

    removed secret key

--- ddos_attack_api/settings.py
+++ ddos_attack_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$qsx@_8w2@_c4@!r7wjzd$2zrfh!)=1$zjg%7_8hnv^egr3w&x'
+SECRET_KEY = 'add-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
